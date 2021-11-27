bdc42360815547123af16a25614e7268dec85d73
Николай Гребенюк <Yanukhous@ukr.net>
2021-11-27 13:34:06 +0200

    delete console.log

--- src/redux/phonebook/reducers.js
+++ src/redux/phonebook/reducers.js
@@ -7,7 +7,6 @@ const contactList = createReducer([], {
     [fetchContacts.fulfilled]: (_, { payload }) => payload,
     [addContact.fulfilled]: (state, { payload }) => [...state, payload],
     [deleteContact.fulfilled]: (state, action) => {
-        console.log(action);
         return state.filter(contact => contact.id !== action.payload);
     },
 });
